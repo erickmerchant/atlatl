db061c6ac21afb0172b2fbb14edf068424d1480d
Erick Merchant <erickmerchant@gmail.com>
2015-08-10 20:06:56 -0700

    Simplify

--- code/index.js
+++ code/index.js
@@ -1,8 +1,101 @@
 'use strict'
 
-const load = require('./load.js')
+const fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
 const make = require('./make.js')
 
 module.exports = function (directory) {
-  return load(directory, make)
+  directory = path.resolve(process.cwd(), directory) + '/'
+
+  const coreFile = directory + 'compiled/.core.js'
+  const compiledDirectory = directory + 'compiled/'
+  var promises = {}
+
+  var corePromise = new Promise(function (resolve, reject) {
+    mkdirp(path.dirname(coreFile), function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        let coreCode = `var safeVals = new Map()
+
+        ${ require('escape-html').toString() }
+
+        module.exports.safe = function safe(val) {
+          var result = Symbol()
+
+          safeVals.set(result, val)
+
+          return result
+        }
+
+        module.exports.escape = function escape (strings) {
+          var values = [].slice.call(arguments, 1)
+          var result = ''
+
+          strings.forEach(function (val, key) {
+            result += val
+
+            if (values[key]) {
+              if (typeof values[key] == 'symbol' && safeVals.has(values[key])) {
+                result += safeVals.get(values[key])
+              } else {
+                result += escapeHtml(values[key])
+              }
+            }
+          })
+
+          return result
+        }`
+
+        fs.writeFile(coreFile, coreCode, function (err) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(coreFile)
+          }
+        })
+      }
+    })
+  })
+
+  return function load (name, callback) {
+    corePromise.then(function () {
+      if (!promises[name]) {
+        promises[name] = new Promise(function (resolve, reject) {
+          fs.readFile(directory + name, { encoding: 'utf-8' }, function (err, template) {
+            if (err) {
+              reject(err)
+            } else {
+              make(name, template, compiledDirectory, load, function (err, result) {
+                if (err) {
+                  reject(err)
+                } else {
+                  mkdirp(path.dirname(compiledDirectory + name + '.js'), function (err) {
+                    if (err) {
+                      reject(err)
+                    } else {
+                      fs.writeFile(compiledDirectory + name + '.js', result.join('\n'), function (err) {
+                        if (err) {
+                          reject(err)
+                        } else {
+                          resolve(compiledDirectory + name + '.js')
+                        }
+                      })
+                    }
+                  })
+                }
+              })
+            }
+          })
+        })
+      }
+
+      promises[name].then(function (path) {
+        var template = require(path)
+
+        callback(null, template)
+      }).catch(callback)
+    })
+  }
 }
--- code/make.js
+++ code/make.js
@@ -1,256 +1,221 @@
 'use strict'
 
-module.exports = function (directory, read, load) {
-  return function (name, callback) {
-    make(name, function (err, code) {
-      if (err) {
-        callback(err)
-      } else {
-        callback(null, code)
-      }
+module.exports = function (name, template, directory, load, callback) {
+  var lines = template.split(/\s*(@.*)\n/g)
+  var renderCode = []
+  var extending = false
+  var dependencies = []
+  var sections = []
+  var partials = []
+  var imports = []
+  var vars = []
+  var ends = []
+
+  renderCode = renderCode.concat(compile(lines))
+
+  Promise.all(dependencies).then(function () {
+
+    if (!extending) {
+      sections.push(`render(content) {
+        var output = []
+
+        ${ renderCode.join('\n') }
+
+        return output.join('\\n')
+      }`)
+    }
+
+    var code = []
+
+    code.push('"use strict"')
+
+    code.push('var escape = require("' + directory + '.core.js").escape')
+    code.push('var safe = require("' + directory + '.core.js").safe')
+
+    Object.keys(imports).forEach(function (k) {
+      code.push('var ' + k + ' = require("' + directory + imports[k] + '.js").partials.' + k)
     })
-  }
 
-  function make (name, callback) {
-    read(name, function (err, template) {
-      var lines = template.split(/\s*(@.*)\n/g)
-      var renderCode = []
-      var extending = false
-      var dependencies = []
-      var sections = []
-      var partials = []
-      var imports = []
-      var vars = []
-      var ends = []
+    if (vars.length) {
+      code.push('var ' + vars.join(', '))
+    }
 
-      if (err) {
-        callback(err)
-      } else {
-        renderCode = renderCode.concat(compile(lines))
+    if (extending) {
+      code.push('var ParentSections = require("' + directory + extending + '.js").Sections')
+    }
 
-        Promise.all(dependencies).then(function () {
-          var codePromise = Promise.resolve(renderCode)
+    code.push('class Sections' + (extending ? ' extends ParentSections' : '') + ' {')
 
-          if (extending) {
-            codePromise = new Promise(function (resolve, reject) {
-              make(extending, function (err, renderCode) {
-                if (err) {
-                  reject(err)
-                } else {
-                  resolve()
+    code = code.concat(sections)
+
+    code.push('}')
+
+    code = code.concat(Object.keys(partials).map(function (k) {
+      return partials[k]
+    }))
+
+    code.push('function render(content) { return (new Sections()).render(content) }')
+
+    code.push('render.Sections = Sections')
+
+    code.push('render.partials = {}')
+
+    Object.keys(partials).forEach(function (k) {
+      code.push('render.partials.' + k + ' = ' + k)
+    })
+
+    code.push('module.exports = render')
+
+    callback(null, code)
+
+  }).catch(callback)
+
+  function compile (lines) {
+    var code = []
+
+    while (lines.length) {
+      let line = lines.shift()
+      let trimmed = line.trim()
+      let parts = trimmed.substr(1).split(/\s+/)
+      let directive = parts[0]
+      let args = parts.slice(1)
+      let arg0 = args[0]
+      let func = []
+      let level
+
+      if (trimmed.startsWith('@')) {
+        if (trimmed === '@') {
+          code.push(ends.shift())
+        } else {
+          if (['section', 'partial'].indexOf(directive) > -1) {
+            level = 1
+            while (level) {
+              let line = lines.shift() || ''
+              let trimmed = line.trim()
+
+              if (trimmed === '@') {
+                level -= 1
+                if (level) {
+                  func.push(line)
                 }
-              })
-            })
+              } else {
+                if (trimmed.startsWith('@') && ['if', 'each', 'section', 'partial'].indexOf(trimmed.substr(1).split(/\s+/)[0]) > -1) {
+                  level += 1
+                }
+
+                func.push(line)
+              }
+            }
           }
 
-          codePromise.then(function (renderCode) {
+          switch (directive) {
+            case 'extends':
+              extending = arg0
+              dependencies.push(new Promise(function (resolve, reject) {
+                load(arg0, function (err) {
+                  if (err) {
+                    reject(err)
+                  } else {
+                    resolve()
+                  }
+                })
+              }))
+              break
 
-            if (!extending) {
-              sections.push(`render(content) {
+            case 'embed':
+              code.push('output.push(require("' + directory + arg0 + '.js")(content))')
+              dependencies.push(new Promise(function (resolve, reject) {
+                load(arg0, function (err) {
+                  if (err) {
+                    reject(err)
+                  } else {
+                    resolve()
+                  }
+                })
+              }))
+              break
+
+            case 'set':
+              code.push(arg0 + ' = ' + args.slice(1).join(' '))
+              vars.push(arg0)
+              break
+
+            case 'yield':
+              code.push('output = output.concat(this.' + arg0 + '(content))')
+              break
+
+            case 'parent':
+              code.push('output = output.concat(super(content))')
+              break
+
+            case 'section':
+              code.push('output = output.concat(this.' + arg0 + '(content))')
+              sections.push(`${ arg0 }(content) {
                 var output = []
-
-                ${ renderCode.join('\n') }
-
-                return output.join('\\n')
+                ${ compile(func).join('\n') }
+                return output
               }`)
-            }
+              break
 
-            var code = []
-
-            code.push('"use strict"')
-
-            code.push('var escape = require("' + directory + '.core.js").escape')
-            code.push('var safe = require("' + directory + '.core.js").safe')
-
-            Object.keys(imports).forEach(function (k) {
-              code.push('var ' + k + ' = require("' + directory + imports[k] + '.js").partials.' + k)
-            })
-
-            if (vars.length) {
-              code.push('var ' + vars.join(', '))
-            }
-
-            if (extending) {
-              code.push('var ParentSections = require("' + directory + extending + '.js").Sections')
-            }
-
-            code.push('class Sections' + (extending ? ' extends ParentSections' : '') + ' {')
-
-            code = code.concat(sections)
-
-            code.push('}')
-
-            code = code.concat(Object.keys(partials).map(function (k) {
-              return partials[k]
-            }))
-
-            code.push('function render(content) { return (new Sections()).render(content) }')
-
-            code.push('render.Sections = Sections')
-
-            code.push('render.partials = {}')
-
-            Object.keys(partials).forEach(function (k) {
-              code.push('render.partials.' + k + ' = ' + k)
-            })
-
-            code.push('module.exports = render')
-
-            callback(null, code)
-
-          }).catch(callback)
-
-        }).catch(callback)
-      }
-
-      function compile (lines) {
-        var code = []
-
-        while (lines.length) {
-          let line = lines.shift()
-          let trimmed = line.trim()
-          let parts = trimmed.substr(1).split(/\s+/)
-          let directive = parts[0]
-          let args = parts.slice(1)
-          let arg0 = args[0]
-          let func = []
-          let level
-
-          if (trimmed.startsWith('@')) {
-            if (trimmed === '@') {
-              code.push(ends.shift())
-            } else {
-              if (['section', 'partial'].indexOf(directive) > -1) {
-                level = 1
-                while (level) {
-                  let line = lines.shift() || ''
-                  let trimmed = line.trim()
-
-                  if (trimmed === '@') {
-                    level -= 1
-                    if (level) {
-                      func.push(line)
-                    }
+            case 'import':
+              imports[arg0] = args.slice(1).join(' ')
+              dependencies.push(new Promise(function (resolve, reject) {
+                load(args.slice(1).join(' '), function (err) {
+                  if (err) {
+                    reject(err)
                   } else {
-                    if (trimmed.startsWith('@') && ['if', 'each', 'section', 'partial'].indexOf(trimmed.substr(1).split(/\s+/)[0]) > -1) {
-                      level += 1
-                    }
-
-                    func.push(line)
+                    resolve()
                   }
-                }
+                })
+              }))
+              break
+
+            case 'partial':
+              if (!partials[arg0]) {
+                partials[arg0] = `function ${ arg0 }(${ args.slice(1).join(', ') }) {
+                  var output = []
+                  ${ compile(func).join('\n') }
+                  return safe(output.join('\\n'))
+                }`
               }
+              break
 
-              switch (directive) {
-                case 'extends':
-                  extending = arg0
-                  dependencies.push(new Promise(function (resolve, reject) {
-                    load(arg0, function (err) {
-                      if (err) {
-                        reject(err)
-                      } else {
-                        resolve()
-                      }
-                    })
-                  }))
-                  break
+            case 'each':
+              code.push('if (Array.isArray(' + arg0 + ') && ' + arg0 + '.length) { output = output.concat(' + arg0 + '.map(function(' + args.slice(1).join(', ') + ') { var output = [] ')
+              ends.unshift("return output.join('\\n') })) }")
+              break
 
-                case 'embed':
-                  code.push('output.push(require("' + directory + arg0 + '.js")(content))')
-                  dependencies.push(new Promise(function (resolve, reject) {
-                    load(arg0, function (err) {
-                      if (err) {
-                        reject(err)
-                      } else {
-                        resolve()
-                      }
-                    })
-                  }))
-                  break
+            case 'if':
+              code.push('if(' + args.join(' ') + ') {')
+              ends.unshift('}')
+              break
 
-                case 'set':
-                  code.push(arg0 + ' = ' + args.slice(1).join(' '))
-                  vars.push(arg0)
-                  break
-
-                case 'yield':
-                  code.push('output = output.concat(this.' + arg0 + '(content))')
-                  break
-
-                case 'parent':
-                  code.push('output = output.concat(super(content))')
-                  break
-
-                case 'section':
-                  code.push('output = output.concat(this.' + arg0 + '(content))')
-                  sections.push(`${ arg0 }(content) {
-                    var output = []
-                    ${ compile(func).join('\n') }
-                    return output
-                  }`)
-                  break
-
-                case 'import':
-                  imports[arg0] = args.slice(1).join(' ')
-                  dependencies.push(new Promise(function (resolve, reject) {
-                    load(args.slice(1).join(' '), function (err) {
-                      if (err) {
-                        reject(err)
-                      } else {
-                        resolve()
-                      }
-                    })
-                  }))
-                  break
-
-                case 'partial':
-                  if (!partials[arg0]) {
-                    partials[arg0] = `function ${ arg0 }(${ args.slice(1).join(', ') }) {
-                      var output = []
-                      ${ compile(func).join('\n') }
-                      return safe(output.join('\\n'))
-                    }`
-                  }
-                  break
-
-                case 'each':
-                  code.push('if (Array.isArray(' + arg0 + ') && ' + arg0 + '.length) { output = output.concat(' + arg0 + '.map(function(' + args.slice(1).join(', ') + ') { var output = [] ')
-                  ends.unshift("return output.join('\\n') })) }")
-                  break
-
-                case 'if':
-                  code.push('if(' + args.join(' ') + ') {')
-                  ends.unshift('}')
-                  break
-
-                case 'elseif':
-                  if (ends.length) {
-                    code.push(ends.shift())
-                  } else {
-                    code.push('if (false) {')
-                  }
-                  code.push('else if(' + args.join(' ') + ') {')
-                  ends.unshift('}')
-                  break
-
-                case 'else':
-                  if (ends.length) {
-                    code.push(ends.shift())
-                  } else {
-                    code.push('if (false) {')
-                  }
-                  code.push('else {')
-                  ends.unshift('}')
-                  break
+            case 'elseif':
+              if (ends.length) {
+                code.push(ends.shift())
+              } else {
+                code.push('if (false) {')
               }
-            }
-          } else if (line.trim().length) {
-            code.push('output.push(escape`' + line.replace('`', '\\`') + '`)')
+              code.push('else if(' + args.join(' ') + ') {')
+              ends.unshift('}')
+              break
+
+            case 'else':
+              if (ends.length) {
+                code.push(ends.shift())
+              } else {
+                code.push('if (false) {')
+              }
+              code.push('else {')
+              ends.unshift('}')
+              break
           }
         }
-
-        return code
+      } else if (line.trim().length) {
+        code.push('output.push(escape`' + line.replace('`', '\\`') + '`)')
       }
-    })
+    }
+
+    return code
   }
 }
