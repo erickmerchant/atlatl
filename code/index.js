3c368afb53d7a7ab515215e8cf87fa9f0b1ebe78
Erick Merchant <erickmerchant@gmail.com>
2015-09-24 00:04:06 -0700

    Work on tests

--- code/index.js
+++ code/index.js
@@ -3,46 +3,37 @@
 const fs = require('fs')
 const path = require('path')
 const mkdirp = require('mkdirp')
-const makeTemplate = require('./make-template.js')
 const assign = require('lodash.assign')
-const defaultDirectives = {}
+const makeTemplate = require('./make-template.js')
+const defaultDirectives = require('./default-directives.js')
 
-;['--', 'call', 'each', 'else', 'extends', 'if', 'import', 'parent', 'partial', 'section', 'yield'].forEach(function (directive) {
-  defaultDirectives[directive] = require('./directives/' + directive + '.js')
-})
-
-module.exports = function (directory, directives) {
+module.exports = function (directory, settings) {
+  settings = settings || {}
   directory = path.resolve(process.cwd(), directory) + '/'
-  directives = assign({}, defaultDirectives, directives || {})
 
-  const compiledDirectory = directory + 'compiled/' + (new Date()).getTime() + '/'
+  var directives = assign({}, defaultDirectives, settings.directives || {})
+  const compiledDirectory = settings.cacheDirectory || directory + 'compiled/' + (new Date()).getTime() + '/'
   var promises = {}
 
   return function load (name) {
     if (!promises[name]) {
       promises[name] = new Promise(function (resolve, reject) {
         fs.readFile(directory + name, { encoding: 'utf-8' }, function (err, result) {
-          if (err) {
-            reject(err)
-          } else {
-            makeTemplate(result, load, directives, function (err, result) {
-              if (err) {
-                reject(err)
-              } else {
-                mkdirp(path.dirname(compiledDirectory + name + '.js'), function (err) {
-                  if (err) {
-                    reject(err)
-                  } else {
-                    fs.writeFile(compiledDirectory + name + '.js', result, function (err) {
-                      if (err) throw err
+          if (err) throw err
 
-                      resolve(compiledDirectory + name + '.js')
-                    })
-                  }
-                })
-              }
+          makeTemplate(result, load, directives, function (err, result) {
+            if (err) throw err
+
+            mkdirp(path.dirname(compiledDirectory + name + '.js'), function (err) {
+              if (err) throw err
+
+              fs.writeFile(compiledDirectory + name + '.js', result, function (err) {
+                if (err) throw err
+
+                resolve(compiledDirectory + name + '.js')
+              })
             })
-          }
+          })
         })
       })
     }
--- test/index.js
+++ test/index.js
@@ -1,30 +1,98 @@
+'use strict'
+
 var mockery = require('mockery')
 var tape = require('tape')
-var path = require('path')
+const NO_ERRORS = 0b0
+const ERROR_READ = 0b1
+const ERROR_MAKE = 0b10
+const ERROR_DIRECTORY = 0b100
+const ERROR_WRITE = 0b1000
+const testError = new Error('testing')
 
 tape('test index.js', function (t) {
-  var directory = './templates/'
+  testIt(t, NO_ERRORS)
+})
+
+tape('test index.js - error on read', function (t) {
+  testIt(t, ERROR_READ)
+})
+
+tape('test index.js - error on make', function (t) {
+  testIt(t, ERROR_MAKE)
+})
+
+mockery.enable({
+  warnOnReplace: false,
+  warnOnUnregistered: false,
+  useCleanCache: true
+})
+
+tape('test index.js - error on make', function (t) {
+  testIt(t, ERROR_DIRECTORY)
+})
+
+tape('test index.js - error on make', function (t) {
+  testIt(t, ERROR_WRITE)
+})
+
+function testIt (t, expectedErrors) {
   var index
   var load
 
-  t.plan(2)
-
-  mockery.enable({
-    warnOnReplace: false,
-    warnOnUnregistered: false
-  })
+  mockery.registerMock('./default-directives.js', {})
 
   mockery.registerMock('fs', {
     readFile: function (file, options, callback) {
-      t.equal(file, path.resolve(process.cwd(), directory) + '/' + 'test')
+      t.equal(file, process.cwd() + '/templates/test.html')
 
       t.looseEqual(options, { encoding: 'utf-8' })
+
+      callback((expectedErrors & ERROR_READ) ? testError : null, 'testing')
+    },
+    writeFile: function (file, result, callback) {
+      t.equal(file, './templates/compiled/test.html.js')
+
+      t.equal(result, 'testing')
+
+      callback((expectedErrors & ERROR_WRITE) ? testError : null, 'testing')
+    }
+  })
+
+  mockery.registerMock('./make-template.js', function (result, load, directives, callback) {
+    t.equal(result, 'testing')
+
+    t.looseEqual(directives, {})
+
+    callback((expectedErrors & ERROR_MAKE) ? testError : null, 'testing')
+  })
+
+  mockery.registerMock('mkdirp', function (directory, callback) {
+    t.equal(directory, './templates/compiled')
+
+    callback((expectedErrors & ERROR_DIRECTORY) ? testError : null)
+  })
+
+  mockery.registerMock('./templates/compiled/test.html.js', class {
+    render (content) {
+      return 'testing render'
     }
   })
 
   index = require('../code/index.js')
 
-  load = index(directory)
+  load = index('./templates/', {cacheDirectory: './templates/compiled/'})
 
-  load('test')
-})
+  load('test.html')
+  .then(function () {
+    mockery.deregisterAll()
+
+    t.end()
+  })
+  .catch(function (err) {
+    t.looseEqual(err, testError)
+
+    mockery.deregisterAll()
+
+    t.end()
+  })
+}
