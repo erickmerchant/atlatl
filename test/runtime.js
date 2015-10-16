3b6874655c74d3fb466aac2df0aac73ccdc1d954
Erick Merchant <erickmerchant@gmail.com>
2015-10-15 23:35:12 -0700

    Test runtime.js

---
+++ test/runtime.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var test = require('tap').test
+
+test('test index.js', function (t) {
+  t.plan(1)
+
+  var runtime = require('../code/runtime.js')
+  var foo = '<>'
+
+  t.equal(runtime.escape`${runtime.safe(foo)} ${foo}`, '<> &lt;&gt;')
+})
